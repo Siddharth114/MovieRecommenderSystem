cdc4db79744a97058865abe8c85d62388ab3bc65
Siddharth <2004sid@gmail.com>
2021-10-25 13:52:02 +0530

    web app

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
 import streamlit as st
+import pandas as pd
+
+st.title('Movie Recommender System')
+
+nav = st.sidebar.radio('Navigation'['Home', 'Contribute'])
+
+if nav == 'Home':
+    st.write('Home')
+if nav == 'Contribute':
+    st.write('Contribute')
+
