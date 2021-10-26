a194643f3d0ab75d687f86a04bcef49232664440
Siddharth <2004sid@gmail.com>
2021-10-26 10:06:54 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ if nav == 'Home':
     option = st.selectbox(
         'Choose your movie',
         (i for i in main.titles))
-    st.write('You selected:')
+    st.subheader('You selected:')
     option_img_response = requests.get(f'http://www.omdbapi.com/?t={option}&apikey=f5f824aa')
     option_img = option_img_response.json()
 
@@ -28,13 +28,22 @@ if nav == 'Home':
         summaries.append((' '.join(str(v) for v in (((main.data.loc[main.data['title'] == i])['overview']).to_list())[0])))
 
 
+    st.subheader('Check out these recommendations:')
 
-    st.write('Check out these recommendations:')
-    st.image(images[0][1]['Poster'], width=200, caption = f'{images[0][0]}: {summaries[0]}')
-    st.image(images[1][1]['Poster'], width=200, caption = f'{images[1][0]}: {summaries[1]}')
-    st.image(images[2][1]['Poster'], width=200, caption = f'{images[2][0]}: {summaries[2]}')
-    st.image(images[3][1]['Poster'], width=200, caption = f'{images[3][0]}: {summaries[3]}')
-    st.image(images[4][1]['Poster'], width=200, caption = f'{images[4][0]}: {summaries[4]}')
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        st.image(images[0][1]['Poster'], width=200, caption = f'{images[0][0]}: {summaries[0]}')
+    with col2:
+        st.image(images[1][1]['Poster'], width=200, caption = f'{images[1][0]}: {summaries[1]}')
+    with col3:
+        st.image(images[2][1]['Poster'], width=200, caption = f'{images[2][0]}: {summaries[2]}')
+
+    col4, col5, col6 = st.columns(3)
+    with col4:
+        st.image(images[3][1]['Poster'], width=200, caption = f'{images[3][0]}: {summaries[3]}')
+    with col5:
+        st.image(images[4][1]['Poster'], width=200, caption = f'{images[4][0]}: {summaries[4]}')
 
 if nav == 'Contribute':
     st.header('Contribute')
@@ -46,3 +55,5 @@ if nav == 'Contribute':
         if submitted:
             st.write('Movie to be updated:', title)
             st.wrte('Your contribution is much appreciated!')
+
+    
